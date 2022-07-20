f71f790c593d4cd1223d39ae1af63681ea3e1adf
hanhqvu <hanhqvu@users.noreply.github.com>
2022-07-20 19:12:33 +0900

    fix: common js syntax

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,8 @@
-import dotenv from "dotenv";
+const dotenv = require("dotenv");
 
 dotenv.config();
 
-export default {
+module.exports = {
     client: "pg",
     connection: process.env.DB_URL || {
         host: process.env.DB_HOST || "127.0.0.1",
@@ -13,6 +13,7 @@ export default {
     },
     migrations: {
         directory: "./db/migrations",
+        loadExtensions: [".js"],
     },
     seeds: {
         directory: "./db/seeds",
