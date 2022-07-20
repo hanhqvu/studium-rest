00fa0ed29e5d35f46933d3235cccf3e708545762
hanhqvu <hanhqvu@users.noreply.github.com>
2022-07-20 20:04:42 +0900

    feat: add get all course endpoint

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const knex = require("../db/knex");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -7,6 +8,16 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello World");
+});
+
+app.get("/api/course", async (req, res) => {
+    console.log(req.body);
+    try {
+        const allCourse = await knex.select("*").from("course");
+        res.send(allCourse).status(200).end();
+    } catch (err) {
+        res.send(404).end();
+    }
 });
 
 app.listen(PORT, async () => {
