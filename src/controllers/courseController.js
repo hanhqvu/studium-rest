d8d9045bfdd7c8df648c827834009a85659e948c
hanhqvu <hanhqvu@users.noreply.github.com>
2022-07-20 21:52:33 +0900

    fix: remove unused variable

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -1,8 +1,6 @@
 const express = require("express");
 
 const router = express.Router();
-
-const knex = require("../../db/knex");
 const courseModel = require("../models/courseModel");
 
 router.get("/", async (req, res) => {
