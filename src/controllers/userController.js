c8a8318de1edb79ade76a54c137472f5f7ac6e62
hanhqvu <hanhqvu@users.noreply.github.com>
2022-07-28 13:04:28 +0900

    feat: add endpoint to find user and handle user auth

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,55 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 const userModel = require("../models/userModel");
 
-router.post("/", async (req, res) => {
+router.get("/", async (req, res) => {
+    try {
+        const allUsers = await userModel.getAll();
+        res.send(allUsers).status(204).end();
+    } catch (err) {
+        res.sendStatus(404).end();
+    }
+});
+
+router.post("/sign-up", async (req, res) => {
     try {
         await userModel.addUser(req.body);
         res.sendStatus(204).end();
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(404).end();
+    }
+});
+
+router.get("/:user_name", async (req, res) => {
+    try {
+        const user = await userModel.getOne(req.params.user_name);
+        res
+            .send(user.user_name)
+            .status(200)
+            .end();
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(404).end();
+    }
+});
+
+router.post("/log-in", async (req, res) => {
+    const { userName, password } = req.body;
+    try {
+        const validate = userModel.validate(userName, password);
+        if (validate) {
+            const token = jwt.sign(
+                { data: userName, exp: 3600 },
+                process.env.PRIVATE_KEY,
+            );
+            res
+                .cookie("token", token, { httpOnly: true })
+                .status(200)
+                .end();
+        }
     } catch (err) {
         console.log(err);
         res.sendStatus(404).end();
