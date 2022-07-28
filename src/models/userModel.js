7a81b1847d8739bf0c2f04343fb3b111abacb106
hanhqvu <hanhqvu@users.noreply.github.com>
2022-07-28 13:03:55 +0900

    feat: add method getOne and validate

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,6 +2,11 @@ const argon2 = require("argon2");
 const knex = require("../../db/knex");
 
 module.exports = {
+    async getAll() {
+        const allUsers = await knex.select("*").from("users");
+        return allUsers;
+    },
+
     async addUser(payload) {
         const { userName, password } = payload;
         const hashedPassword = await argon2.hash(password, {
@@ -14,5 +19,34 @@ module.exports = {
             password: hashedPassword,
         });
         return "Successfully created new user";
+    },
+
+    getOne(userName) {
+        return knex.select("*").from("users").where(
+            "user_name",
+            userName,
+        ).first();
+    },
+
+    async validate(userName, password) {
+        const user = await this.getOne(userName);
+        if (!user) {
+            throw new Error("No user found");
+        }
+        const validation = await argon2.verify(user.password, password);
+        if (!validation) {
+            throw new Error("Password Incorrect");
+        }
+        return validation;
+    },
+
+    async editUser(name, payload) {
+        await knex("users").where("name", name).update(payload);
+        return "Successfully update user";
+    },
+
+    async removeUser(name) {
+        await knex("users").where("name", name).del();
+        return "Successfully delete user";
     },
 };
